8a4ef26b9c3f627942e92b8571eef94a30f86226
Andy Wu <andywu@Andys-Mac.local>
2017-02-06 14:45:29 -0500

    Finished game

--- Avoid Spikes/GameScene.swift
+++ Avoid Spikes/GameScene.swift
@@ -9,81 +9,243 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+var player = SKSpriteNode()
+var spike = SKSpriteNode()
+var ground = SKSpriteNode()
+
+var lblMain = SKLabelNode()
+var lblScore = SKLabelNode()
+
+var spikeSpeed = 2.0
+
+var isAlive = true
+
+var score = 0
+
+var spikeTimeSpawnNumber = 0.4
+
+var offWhiteColor = UIColor(colorLiteralRed: 0.95, green: 0.95, blue: 0.95, alpha: 1.0)
+
+var offBlackColor = UIColor(colorLiteralRed: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
+
+var touchLocation = CGPoint()
+
+
+struct physicsCategory {
+    static let player:UInt32 = 1
+    static let spike:UInt32 = 2
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        physicsWorld.contactDelegate = self
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        self.backgroundColor = .orange
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        spawnPlayer()
+        spawnGround()
+        spawnLblMain()
+        spawnLblScore()
+        spikeSpawnTimer()
+        hideLable()
+        resetVarsOnStart()
+        addToScore()
     }
     
+    func resetVarsOnStart() {
+        score = 0
+        isAlive = true
+    }
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+            touchLocation = touch.location(in: self)
+            
+            if isAlive == true {
+                player.position.x = touchLocation.x
+            }
+            else if isAlive == false {
+                player.position.x = -500
+            }
+        }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        if isAlive == false {
+            player.position.x = -500
+        }
+    }
+    
+    func spawnPlayer() {
+        player = SKSpriteNode(imageNamed: "Player")
+        //player = SKSpriteNode(color: offWhiteColor, size: CGSize(width: 50, height: 50))
+        player.position = CGPoint(x: self.frame.midX, y: (self.frame.size.height) * -0.5 + 130 )
+        
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody?.affectedByGravity = false
+        player.physicsBody?.allowsRotation = false
+        player.physicsBody?.isDynamic = false
+        player.physicsBody?.categoryBitMask = physicsCategory.player
+        player.physicsBody?.contactTestBitMask = physicsCategory.spike
+        
+        self.addChild(player)
+    }
+    
+    func spawnSpike() {
+        spike = SKSpriteNode(imageNamed: "Spike")
+        //spike = SKSpriteNode(color: offBlackColor, size: CGSize(width: 10, height: 125))
+        
+        let ranNegXPos:Int = (Int(arc4random_uniform(UInt32(self.frame.width))) * -1 )
+        let ranPositXPos:Int = Int(arc4random_uniform(UInt32(self.frame.width)))
+        
+        
+        spike.position.x = CGFloat(ranNegXPos + ranPositXPos)
+        spike.position.y = 900
+        
+        spike.physicsBody = SKPhysicsBody(rectangleOf: spike.size)
+        spike.physicsBody?.affectedByGravity = false
+        spike.physicsBody?.allowsRotation = false
+        spike.physicsBody?.categoryBitMask = physicsCategory.spike
+        spike.physicsBody?.contactTestBitMask = physicsCategory.player
+        spike.physicsBody?.isDynamic = true
+        
+        var moveDown = SKAction.moveTo(y: -500, duration: spikeSpeed)
+        
+        if isAlive == true {
+            moveDown = SKAction.moveTo(y: -500, duration: spikeSpeed)
+        }
+        else if isAlive == false {
+            moveDown = SKAction.moveTo(y: 2000, duration: spikeSpeed)
+        }
+        
+        
+        spike.run(moveDown)
+        
+        self.addChild(spike)
+        
+    }
+    
+    func spawnGround() {
+        ground = SKSpriteNode(color: offBlackColor, size: CGSize(width: self.frame.width, height: 200))
+        ground.position = CGPoint(x: self.frame.midX, y: self.frame.minY)
+        
+        self.addChild(ground)
+    }
+    
+    func spawnLblMain (){
+        lblMain = SKLabelNode(fontNamed: "Futura")
+        lblMain.fontSize = 100
+        lblMain.fontColor = offWhiteColor
+        lblMain.position = CGPoint(x: self.frame.midX, y: self.frame.midY + 150)
+        
+        lblMain.text = "Start!"
+        
+        self.addChild(lblMain)
+    }
+    
+    func spawnLblScore () {
+        lblScore = SKLabelNode(fontNamed: "Futura")
+        lblScore.fontSize = 40
+        lblScore.fontColor = offWhiteColor
+        lblScore.position = CGPoint(x: self.frame.midX, y: (self.frame.size.height) * -0.5 + 20 )
+        
+        lblScore.text = "Score: 0"
+        
+        self.addChild(lblScore)
+        
+    }
+    
+    func spikeSpawnTimer() {
+        let spikeTimer = SKAction.wait(forDuration: spikeTimeSpawnNumber)
+        
+        let spawn = SKAction.run {
+            self.spawnSpike()
+        }
+        
+        let sequence = SKAction.sequence([spikeTimer, spawn])
+        
+        self.run(SKAction.repeatForever(sequence))
+    }
+    
+    func hideLable() {
+        let wait = SKAction.wait(forDuration: 3.0)
+        let hideLabel = SKAction.run {
+            lblMain.alpha = 0.0
+        }
+        
+        let sequence = SKAction.sequence([wait, hideLabel])
+        
+        self.run(SKAction.repeat(sequence, count: 1))
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let firstBody:SKPhysicsBody = contact.bodyA
+        let secondBody:SKPhysicsBody = contact.bodyB
+        
+        if(((firstBody.categoryBitMask == physicsCategory.player) && (secondBody.categoryBitMask == physicsCategory.spike)) || ((firstBody.categoryBitMask == physicsCategory.spike) && (secondBody.categoryBitMask == physicsCategory.player))) {
+            spikeCollision(playerTemp: firstBody.node as! SKSpriteNode, spikeTemp: secondBody.node as! SKSpriteNode)
+            
+        }
+    }
+    
+    func spikeCollision(playerTemp:SKSpriteNode, spikeTemp: SKSpriteNode) {
+        spikeTemp.removeFromParent()
+        
+        lblMain.alpha = 1.0
+        lblMain.fontSize = 50
+        lblMain.text = "GAME OVER"
+        
+        isAlive = false
+        
+        updateScore()
+        waitThenMoveToTitleScene()
+    }
+    
+    func updateScore() {
+        lblScore.text = "Score: \(score)"
+    }
+    
+    func waitThenMoveToTitleScene() {
+        let wait = SKAction.wait(forDuration: 1.0)
+        
+        let transition = SKAction.run {
+            self.view?.presentScene(TitleScene(), transition: SKTransition.crossFade(withDuration: 1.0))
+            
+        }
+        
+        let sequence = SKAction.sequence([wait, transition])
+        
+        self.run(SKAction.repeat(sequence, count: 1))
+    }
+    
+    func addToScore() {
+        let timeInterval = SKAction.wait(forDuration: 1.0)
+        let addAndUpdateScore = SKAction.run {
+            score += 1
+            self.updateScore()
+        }
+        
+        let sequence = SKAction.sequence([timeInterval, addAndUpdateScore])
+        
+        self.run(SKAction.repeatForever(sequence))
     }
 }
---
+++ Avoid Spikes/TitleScene.swift
@@ -0,0 +1,62 @@
+//
+//  TitleScene.swift
+//  Avoid Spikes
+//
+//  Created by Andy Wu on 2/6/17.
+//  Copyright © 2017 Andy Wu. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class TitleScene: SKScene {
+    
+    
+    var btnPlay : UIButton!
+    var gameTitle : UILabel!
+    
+    override func didMove(to view: SKView) {
+        self.backgroundColor = .orange
+        
+        setUpText()
+    }
+    
+    func setUpText(){
+        
+        btnPlay = UIButton(frame: CGRect(x: 100, y: 100, width: 400, height: 100))
+        btnPlay.center = CGPoint(x: (self.view?.frame.size.width)! / 2, y: (self.view?.frame.size.height)! / 2)
+        
+        btnPlay.titleLabel?.font = UIFont(name: "Futura", size: 40)
+        
+        btnPlay.setTitle("Play!", for: UIControlState.normal)
+        btnPlay.setTitleColor(offBlackColor, for: UIControlState.normal)
+        
+        btnPlay.addTarget(self, action: #selector(TitleScene.playTheGame), for: UIControlEvents.touchUpInside)
+        
+        self.view?.addSubview(btnPlay)
+        
+        gameTitle = UILabel(frame: CGRect(x: 0, y: 0, width: view!.frame.width, height: 300))
+        gameTitle.textColor = offWhiteColor
+        gameTitle.font = UIFont(name: "Futura", size: 50)
+        gameTitle.textAlignment = NSTextAlignment.center
+        gameTitle.text = "AVOID SPIKES!"
+        
+        self.view?.addSubview(gameTitle)
+    }
+    
+    func playTheGame() {
+        self.view?.presentScene(GameScene(), transition: SKTransition.crossFade(withDuration: 1.0))
+        
+        btnPlay.removeFromSuperview()
+        gameTitle.removeFromSuperview()
+        
+        if let scene = GameScene(fileNamed: "GameScene") {
+            let skView = self.view! as SKView
+            skView.ignoresSiblingOrder = true
+            scene.size = (view?.bounds.size)!
+            scene.scaleMode = .resizeFill
+            skView.presentScene(scene)
+            
+        }
+    }
+}
